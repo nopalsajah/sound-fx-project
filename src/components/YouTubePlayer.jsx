94f90331116759c64a76293d9d98fa986b38f777
nopalsajah <fitodna281@gmail.com>
2025-03-18 21:16:22 +0700

    Update package.json, YouTubePlayer.jsx, dan tambahkan file baru

--- src/components/YouTubePlayer.jsx
+++ src/components/YouTubePlayer.jsx
@@ -37,7 +37,7 @@ const YouTubePlayer = () => {
           <iframe
             width="560"
             height="315"
-            src={\`https://www.youtube.com/embed/\${videoId}\`}
+            src={`https://www.youtube.com/embed/${videoId}`}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
